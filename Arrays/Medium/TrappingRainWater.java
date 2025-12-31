041dd304d7e45775303c471ce8398487f7b0b2c9
Thulasi <83580674+Thulasi-dev@users.noreply.github.com>
2025-12-31 18:07:48 +0530

    Create TrappingRainWater.java

---
+++ Arrays/Medium/TrappingRainWater.java
@@ -0,0 +1,40 @@
+/* Given an array height[] where each element represents bar height,
+find how much rainwater can be trapped after raining.
+
+✅ Optimal Solution – Two Pointer Technique
+⏱ Time Complexity: O(n)
+💾 Space Complexity: O(1)
+
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height)); // Output: 6
+    }
+}
